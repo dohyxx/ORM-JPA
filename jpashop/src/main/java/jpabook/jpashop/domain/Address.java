41b55419bd4e70eeee381bf7606e7f2b9d7fabb0
dohyun <dohyun1225@gmail.com>
2021-12-31 01:10:46 +0900

    12/31 엔티티 클래스 개발2

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -11,4 +11,14 @@ public class Address {
     private String street;
     private String zipcode;
 
+    //접근제어자 설정
+    protected Address() {
+    }
+
+    //생성할때만 값이 setting, Setter를 제공하지 않는다. 변경불가
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
