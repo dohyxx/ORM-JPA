24f3c505d5557172d5428ca2ae7a5b46e490f1d3
dohyun <dohyun1225@gmail.com>
2022-01-04 02:02:43 +0900

    1/4 주문 도메인 개발1

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -26,4 +26,40 @@ public class OrderItem {
 
     private int orderPrice;//주문 가격
     private int count; //주문 수량
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() { //취소 시 재고 상태 원상복귀
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    /**
+     * 주문상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
